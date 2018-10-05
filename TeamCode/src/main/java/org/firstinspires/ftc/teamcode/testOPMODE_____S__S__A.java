455b3be6d7a54e330ad763366ef6a9526a4d94c0
Sarah Pambianchi <Sarahpambi76@Sarahs-MacBook-Pro.local>
2018-10-04 20:48:42 -0400

    Install EnderCV and stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOPMODE_____S__S__A.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOPMODE_____S__S__A.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import android.hardware.Sensor;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -21,6 +23,7 @@ public class testOPMODE_____S__S__A extends LinearOpMode {
     private DcMotor mtrBL;
 
     private DistanceSensor sensorColorRange;
+    private BNO055IMU imu;
 
     private double yPower;
     private double xPower;
@@ -28,17 +31,30 @@ public class testOPMODE_____S__S__A extends LinearOpMode {
     private double rightPower;
     private double leftPower;
 
+
     @Override
     public void runOpMode() {
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
         mtrFR = hardwareMap.get(DcMotor.class,"mtrFR");
         mtrFL = hardwareMap.get(DcMotor.class,"mtrFL");
         mtrBR = hardwareMap.get(DcMotor.class,"mtrBR");
         mtrBL = hardwareMap.get(DcMotor.class,"mtrBL");
 
         sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+
 
         waitForStart();
 
