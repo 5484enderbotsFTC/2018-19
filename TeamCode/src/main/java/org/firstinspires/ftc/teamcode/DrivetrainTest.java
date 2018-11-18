cc03c010361f4c9710c28bc85b34882d19bbe525
Avery Pambianchi <averypambianchi@me.com>
2018-11-18 12:50:53 -0500

    ENCODER TEST

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivetrainTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivetrainTest.java
@@ -3,8 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
 import org.firstinspires.ftc.teamcode.util.Encoder;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 @Autonomous (name = "DriveTrainTest")
 public class DrivetrainTest extends LinearOpMode {
@@ -14,11 +18,70 @@ public class DrivetrainTest extends LinearOpMode {
     DcMotor mtrBL;
     Encoder encFL;
 
+
     @Override
     public void runOpMode(){
         mtrBL = hardwareMap.dcMotor.get("mtrBL");
         mtrBR = hardwareMap.dcMotor.get("mtrBR");
         mtrFL = hardwareMap.dcMotor.get("mtrFL");
         mtrFR = hardwareMap.dcMotor.get("mtrFR");
+        encFL = new Encoder(mtrFL);
+        encFL.resetEncoder();
+        waitForStart();
+
+        appendLog("dab");
+        mtrBL.setPower(1);
+        mtrBR.setPower(1);
+        float t;
+        for (int i=0;i<11;i++){
+            t=i/2;
+
+            appendLog("time:");
+            appendLog(t.toString());
+            appendLog(encFL.getEncValue().toString());
+            sleep(500);
+        }
+        mtrBL.setPower(1);
+        mtrBR.setPower(1);
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public void appendLog(String text)
+    {
+        File logFile = new File("sdcard/log.file");
+        if (!logFile.exists())
+        {
+            try
+            {
+                logFile.createNewFile();
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        try
+        {
+            //BufferedWriter for performance, true to set append to file flag
+            BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true));
+            buf.append(text);
+            buf.newLine();
+            buf.close();
+        }
+        catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
